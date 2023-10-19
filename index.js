465a3c8b3be0b7617905d277ea9a232f92956a8c
taijulsir <princetaijul123@gmail.com>
2023-10-19 23:43:10 +0600

    Read every single product data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,34 +29,39 @@ async function run() {
 
     const brandCarsCollection = client.db("brandsCardDB").collection('brandsCars')
     const brandsNameCollection = client.db("brandsCardDB").collection('brandNames')
-    const brandsSldierCollection = client.db("brandsCardDB").collection('brandSlider')
- 
-
+    
     //  create data from client server 
-
     app.post('/brandNames', async (req, res) => {
       const brands = req.body;
       const result = await brandCarsCollection.insertOne(brands)
       res.send(result)
     })
 
-// load all 6 brands name and images
-    app.get('/brandNames', async (req, res) => {
-      const brands = req.body;
-     const cursor =  brandsNameCollection.find()
-     const result = await cursor.toArray()
-     res.send(result)
-    })
-
-    // load single brands slider
-    app.get('/brandNames/:brandName', async(req,res)=>{
+    // Read multiple product brand wise
+    app.get('/brandNames/:brandName', async (req, res) => {
       const name = req.params.brandName;
-      const query = { brandName: name}
-      const result = await brandsSldierCollection.findOne(query);
+      const query = { brandName: name }
+      const cursor = brandCarsCollection.find(query)
+      const result = await cursor.toArray()
       res.send(result)
     })
-   
 
+    // Read single product brand wise
+    app.get('/cars/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId (id) }
+      console.log(query)
+      const result = await brandCarsCollection.findOne(query)
+      res.send(result)
+    })
+  
+    // load all 6 brands name and images
+    app.get('/brandNames', async (req, res) => {
+      const brands = req.body;
+      const cursor = brandsNameCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
