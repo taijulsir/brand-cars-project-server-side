6027aa2db56c39e996cfcea7ea9cd66facdd91d1
taijulsir <princetaijul123@gmail.com>
2023-10-20 18:13:27 +0600

    update vercel json file and cors

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-
+app.use(cors())
+app.use(express.json())
 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://brandcars8057:I7R5U8diheqH3tCn@cluster0.49cfwvw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  } 
 });
 
 async function run() {
@@ -25,7 +26,7 @@ async function run() {
 
   try {
 
-    await client.connect();
+    // await client.connect();
 
     const brandCarsCollection = client.db("brandsCardDB").collection('brandsCars')
     const brandsNameCollection = client.db("brandsCardDB").collection('brandNames')
@@ -119,7 +120,7 @@ async function run() {
       res.send(result)
     })
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
@@ -129,8 +130,7 @@ async function run() {
 run().catch(console.dir);
 
 
-app.use(cors())
-app.use(express.json())
+
 
 app.get('/', async (req, res) => {
   res.send('The brand cars server is running')
